9508e5cb72492f743763efe96280421c52da74f5
danamestrial <chayapolbun@gmail.com>
2023-01-27 17:37:47 +0700

    feat: Added hist_lib function tests

--- tests/test_histlib.py
+++ tests/test_histlib.py
@@ -1,4 +1,4 @@
-from automated_clean_code.exercise_20_histlib import find_min_max_key, hist_filler
+from automated_clean_code.exercise_20_histlib import find_min_max_key, hist_filler, hist_lib
 
 
 def test_hist_filler():
@@ -10,3 +10,9 @@ def test_find_min_max_key():
     sol = ("l", "h")
     hist = {"h": 1, "e": 1, "l": 2, "o": 1}
     assert find_min_max_key(hist) == sol
+
+
+def test_hist_lib(capsys: object):
+    hist_lib("hello")
+    captured = capsys.readouterr()
+    assert captured.out == "Min Key = h with count = 1 \nMax Key = l with count = 2\n"
